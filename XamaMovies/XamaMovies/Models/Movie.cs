d068c4fc0efda27f023d3d784374ff958a3d834c
Thomas Henry <thomashenry@hotmail.fr>
2020-02-21 12:12:06 +0100

    ajout des modèles de données

---
+++ XamaMovies/XamaMovies/Models/Movie.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamaMovies.Models
+{
+    public class Movie
+    {
+        public string ImdbId { get; set; }
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Plot { get; set; }
+        public string Released { get; set; }
+        public string Runtime { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public string Actors { get; set; }
+        public string Poster { get; set; }
+        public List<Rating> Ratings { get; set; }
+        public string ImdbRating { get; set; }
+        public string ImdbVotes { get; set; }
+        public string Response { get; set; }
+    }
+}
